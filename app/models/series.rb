a7a48ac42b82d409a1fbfd4882d794e19b19747c
JamesACM <jaimecanalesmarin@gmail.com>
2018-10-23 22:52:56 -0300

    validate size img

--- app/models/series.rb
+++ app/models/series.rb
@@ -16,6 +16,7 @@ class Series < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length:  {in: 3..100}, format: { with: /\A[a-zA-Z0-9_ ]+\z/, message: 'letters and numbers with space' }
   validates_presence_of :release_date
   validates :description, presence: true, length:  {in: 3..300}
+  validates :img, file_size: { less_than: 2.megabytes }
   before_validation :capitalize
 
 
