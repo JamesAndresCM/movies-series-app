96540d6988b27fbc8dd29b2322bdf5d50ab2a716
JamesACM <jaimecanalesmarin@gmail.com>
2018-10-26 02:04:24 -0300

    sanitize description

--- app/serializers/series_serializer.rb
+++ app/serializers/series_serializer.rb
@@ -1,7 +1,12 @@
 class SeriesSerializer < ActiveModel::Serializer
+  include ActionView::Helpers::SanitizeHelper
   attributes :id, :name, :img, :release_date, :description
   has_many :videos
   has_many :tags
   belongs_to :category
   belongs_to :type
+
+  def description
+    strip_tags(object.description.html_safe)
+  end
 end
