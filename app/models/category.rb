293551fd1f449ec7b94d4b29d96f1242f3f6dbf5
JamesACM <jaimecanalesmarin@gmail.com>
2018-10-26 21:12:06 -0300

    add concern

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,14 +1,11 @@
 class Category < ApplicationRecord
+  include Concerns::CapitalizeName
   has_many :series, dependent: :destroy
 
   extend FriendlyId
   friendly_id :name, use: :slugged
   validates :name, presence: true, uniqueness: true
   before_validation :capitalize
-
-  def capitalize
-    self.name = self.name.capitalize if self.name?
-  end
 
   def should_generate_new_friendly_id?
     name_changed?
