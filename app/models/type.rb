5d0e3fac0065cac2cf9fd9dac7312dd058227e8e
JamesACM <jaimecanalesmarin@gmail.com>
2018-10-26 21:12:39 -0300

    add concern

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,9 +1,6 @@
 class Type < ApplicationRecord
+  include Concerns::CapitalizeName
   has_one :serie, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   before_validation :capitalize
-
-  def capitalize
-    self.name = self.name.capitalize if self.name?
-  end
 end
