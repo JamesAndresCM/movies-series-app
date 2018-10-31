3fae46cbc4f4492eea7876a7e087302ae05c56ae
JamesACM <jaimecanalesmarin@gmail.com>
2018-10-31 00:28:33 -0300

    refactor model video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,11 +1,7 @@
 class Video < ApplicationRecord
   belongs_to :series
   mount_uploader :video, VideoUploader
-  validates :name, presence: true, 
-            length:  {in: 3..20}, 
-            format: { with: /\A[a-zA-Z0-9_ ]+\z/, message: 'letters and numbers with space' }
-  validates :video, presence: true,  on: :create
-
+  include Validators::VideosValidator
   process_in_background :video
   store_in_background :video
 end
