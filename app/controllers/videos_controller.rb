3fbdfc831cf0832dc6d3490f965caa8e618e236b
JamesACM <jaimecanalesmarin@gmail.com>
2018-10-31 00:27:06 -0300

    refactor videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
 class VideosController < ApplicationController
   before_action :set_video
   before_action :set_serie
 
   def show
-    page = params[:page].to_i if params[:page]
-    if page.blank? or page == 0
+    page = params[:page].to_i
+    if page.blank? || page.zero?
       redirect_back(fallback_location: series_path(params[:series_id]))
-    else 
-      @videos = @serie.videos.paginate(page: page, per_page: 1).order(created_at: :asc)
+    else
+      @videos = @serie.videos.paginate(page: page, per_page: 1)
+                      .order(created_at: :asc)
       if @videos.empty?
         flash[:notice] = "Page #{params[:page]} not found"
         redirect_back(fallback_location: series_path(params[:series_id]))
@@ -15,19 +18,18 @@ class VideosController < ApplicationController
     end
   end
 
+  private
 
-  private 
+  def set_video
+    @video = Video.find(params[:id])
+  rescue StandardError
+    flash[:notice] = "Video #{params[:id]} not found"
+    redirect_back(fallback_location: series_path(params[:series_id]))
+  end
 
-    def set_video
-      @video = Video.find(params[:id])
-      rescue => exception
-        flash[:notice] = "Video #{params[:id]} not found"
-        redirect_back(fallback_location: series_path(params[:series_id]))
-    end
-
-    def set_serie
-      @serie = Series.friendly.find(params[:series_id])
-      rescue => exception
-        redirect_back(fallback_location: series_path(params[:series_id]))
-    end
+  def set_serie
+    @serie = Series.friendly.find(params[:series_id])
+  rescue StandardError
+    redirect_back(fallback_location: series_path(params[:series_id]))
+  end
 end
