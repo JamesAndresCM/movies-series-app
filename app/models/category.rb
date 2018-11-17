0351ea4cac9e0ebdb02cf7f646ce2a8a07758ed3
JamesACM <jaimecanalesmarin@gmail.com>
2018-11-17 06:50:11 -0300

    case sensitive name

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ApplicationRecord
 
   extend FriendlyId
   friendly_id :name, use: :slugged
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   before_validation :capitalize
 
   def should_generate_new_friendly_id?
