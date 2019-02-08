53017fbecadd580a2412ee6f245c72cc6696ee20
JamesACM <jaimecanalesmarin@gmail.com>
2019-02-08 00:06:50 -0300

    feat: add slug name series

--- app/helpers/series_helper.rb
+++ app/helpers/series_helper.rb
@@ -4,9 +4,13 @@ module SeriesHelper
   def series_links(series)
     if user_signed_in?
       raw("<h5 style='text-align: center;'>") +
-        (link_to fa_icon('pencil-square-o', style: 'color: white; margin: 5px'), edit_series_path(series)) +
-        (link_to fa_icon('trash'), series, method: :delete, data: { confirm: 'Are you sure?' }, style: 'color: red') +
+        (link_to fa_icon('pencil-square-o', style: 'color: white; margin: 5px'), edit_series_path(get_slug(series))) +
+        (link_to fa_icon('trash'), series_path(get_slug(series)), method: :delete, data: { confirm: 'Are you sure?' }, style: 'color: red') +
         raw('</h5>')
       end
+  end
+
+  def get_slug(series)
+    series.is_a?(Series) ? series : series.id 
   end
 end
